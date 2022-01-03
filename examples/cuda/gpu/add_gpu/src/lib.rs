a92ebc549fd84f6c9821dd52c49c0ba04858040d
Andrey Chursin <andll@danasoft.ws>
2022-01-03 05:45:32 +0000

    Test ed25519 verification kernel

--- examples/cuda/gpu/add_gpu/src/lib.rs
+++ examples/cuda/gpu/add_gpu/src/lib.rs
@@ -16,3 +16,19 @@ pub unsafe fn add(a: &[f32], b: &[f32], c: *mut f32) {
         *elem = a[idx] + b[idx];
     }
 }
+
+use ed25519_dalek::{Signature, PublicKey, Verifier};
+
+type Message = [u8; 32];
+
+#[kernel]
+#[allow(improper_ctypes_definitions, clippy::missing_safety_doc)]
+pub unsafe fn verify(a: &[Signature], b: &[PublicKey], d: &[Message], c: *mut u8) {
+    let idx = thread::index_1d() as usize;
+    if idx < a.len() {
+        let elem = &mut *c.add(idx);
+	let res = b[idx].verify(&d[idx], &a[idx]);
+        *elem = if res.is_err() {1} else {2};
+    }
+}
+
